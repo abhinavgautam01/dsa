93a3b428ed266045643a76a7b738bd73c18fa07f
abhinavgautam01 <abgautam1017@gmail.com>
2024-11-08 13:09:47 +0530

    dsa

---
+++ linked_list.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for the linked list node
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+// Create a new node
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Display the linked list
+void display(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// search
+struct Node *search(struct Node *head, int key)
+{
+    struct Node *current = head;
+    while (current != NULL)
+    {
+        if (current->data == key)
+        {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+int main()
+{
+    struct Node *head;
+
+    head = createNode(4);
+    head->next = createNode(5);
+    head->next->next = createNode(6);
+    
+    display(head);
+    
+    int find;
+    printf("Enter element to be search :");
+    scanf("%d", &find);
+    
+    // search(head, find);
+    struct Node *result = search(head, find);
+    if (result)
+    {
+        printf("Found %d in List 1.\n", find);
+    }
+    else
+    {
+        printf("%d not found in List 1.\n", find);
+    }
+
+}
